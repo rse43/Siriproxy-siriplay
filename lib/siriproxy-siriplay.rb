adc9ccdfdee83b799a00e52ac59046b9bfc7002a
Shuo Ran <rse43.rse43@gmail.com>
2012-01-02 17:19:33 +1100

    oops, change the object name accordingly

--- lib/siriproxy-siriplay.rb
+++ lib/siriproxy-siriplay.rb
@@ -10,7 +10,7 @@ require 'pp'
 # Remember to add other plugins to the "config.yml" file if you create them!
 ######
 
-class SiriProxy::Plugin::Example < SiriProxy::Plugin
+class SiriProxy::Plugin::Siriplay < SiriProxy::Plugin
   def initialize(config)
     #if you have custom configuration options, process them here!
   end
